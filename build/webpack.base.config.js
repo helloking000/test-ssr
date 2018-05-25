6154677aa49fc98da8fd204c5c614f4de2448fdb
wang tengyun <wangtengyun@meituan.com>
2018-05-25 12:47:25 +0800

    fiux

---
+++ build/webpack.base.config.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+    devtool: '#cheap-module-source-map',
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/distPublic/',
+        filename: '[name].[chunkhash].js'
+    },
+    module: {
+        noParse: /es6-promise\.js$/, // avoid webpack shimming process
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    compilerOptions: {
+                        preserveWhitespace: false
+                    }
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.styl(us)?$/,
+                use: isProd
+                    ? ExtractTextPlugin.extract({
+                        use: [
+                            {
+                                loader: 'css-loader',
+                                options: { minimize: true }
+                            },
+                            'stylus-loader'
+                        ],
+                        fallback: 'vue-style-loader'
+                    })
+                    : ['vue-style-loader', 'css-loader', 'stylus-loader']
+            },
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new ExtractTextPlugin({
+            filename: 'common.[chunkhash].css'
+        })
+    ],
+}
